622413ffc1a54fac2da81285ba44d03928f69ea2
Love Borgström <loveborgstrom98@gmail.com>
2016-10-14 23:33:10 +0200

    Complete

---
+++ VersionCompare.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# License: The MIT License (MIT)
+# Author Zuzzuc https://github.com/Zuzzuc/
+if [[ $1 == $2 ]];then
+	exit 0
+fi
+local IFS=.
+local i ver1=($1) ver2=($2)
+for ((i=${#ver1[@]}; i<${#ver2[@]}; i++));do
+    ver1[i]=0
+done
+for ((i=0; i<${#ver1[@]}; i++));do
+    if [[ -z ${ver2[i]} ]];then
+        ver2[i]=0
+    fi
+    if ((10#${ver1[i]} > 10#${ver2[i]}));then
+		exit 1
+    fi
+    if ((10#${ver1[i]} < 10#${ver2[i]}));then
+        exit 2
+    fi
+done
+exit
